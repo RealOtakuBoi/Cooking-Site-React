656ada70d23efa06b1c773fb2c719b8288930ef9
realotakuboi <sohamseth2005@gmail.com>
2022-07-24 20:34:42 +0530

    first Commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import Popular from "../Components/Popular"
+import Veggie from "../Components/Veggie"
+
+function Home() {
+  return (
+    <div>
+        <Veggie />
+        <Popular />
+
+    </div>
+  )
+}
+
+export default Home
