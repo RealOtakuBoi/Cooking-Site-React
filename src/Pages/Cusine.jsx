c1cb4d4c643191eb937dc79c1f4333884edd1ec8
realotakuboi <sohamseth2005@gmail.com>
2022-07-25 15:22:01 +0530

    Routes added

--- src/Pages/Cusine.jsx
+++ src/Pages/Cusine.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import {motion} from 'framer-motion';
+import {Link, useParams} from 'react-router-dom';
 
 function Cusine() {
+
+  const [cusine, setCusine] = useState([]);
+  let params = useParams();
+  
+  const getCusine = async (name) => {
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}`);
+    const recipes = await data.json();
+    setCusine(recipes.result);
+  }
+
+  useEffect(() => {
+    getCusine(params.type);
+  },[params.type])
   return (
-    <div>Cusine</div>
+    <div>
+
+    </div>
   )
 }
 
