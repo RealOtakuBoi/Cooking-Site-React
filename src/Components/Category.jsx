a878a65cb384e7ecf4465f00ebf5b4188b165762
realotakuboi <sohamseth2005@gmail.com>
2022-07-25 16:53:02 +0530

    Nav Buttons Done

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -9,22 +9,22 @@ import React from 'react'
 function Category() {
   return (
     <List>
-        <NavLink to={'/cusine/Italian'}>
+        <Slink to={'/cusine/Italian'}>
             <FaPizzaSlice/>
             <h4>Italian</h4>
-        </NavLink>
-        <NavLink to={'/cusine/American'}>
+        </Slink>
+        <Slink to={'/cusine/American'}>
             <FaHamburger/>
             <h4>American</h4>
-        </NavLink>
-        <NavLink to={'/cusine/Chinese'}>
+        </Slink>
+        <Slink to={'/cusine/Chinese'}>
             <GiNoodles/>
             <h4>Chinese</h4>
-        </NavLink>
-        <NavLink to={'/cusine/Japanese'}>
+        </Slink>
+        <Slink to={'/cusine/Japanese'}>
             <GiChopsticks/>
             <h4>Japanese</h4>
-        </NavLink>
+        </Slink>
     </List>
   )
 }
@@ -33,6 +33,45 @@ const List = styled.div`
     display:flex;
     justify-content:center;
     margin: 2rem 0rem;
+    `;
+
+
+const Slink = styled(NavLink)`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 6rem;
+    height: 6rem;
+    border-radius:50%;
+    margin-right: 2rem;
+    text-decoration: none;
+    cursor:pointer;
+    background: #131313;
+    transform: scale(0.7);
+    
+    
+    h4{
+        font-size: 0.8rem;
+    }
+
+    svg{
+        color: #FFD700;
+        font-size: 2.5rem;
+    }
+
+    &.active{
+        background: #FFD700;
+
+        svg{
+            color: black;
+        }
+
+        h4{
+            color: black;
+        }
+    }
+    
     `;
 
 export default Category
--- src/Pages/Cusine.jsx
+++ src/Pages/Cusine.jsx
@@ -11,17 +11,49 @@ function Cusine() {
   const getCusine = async (name) => {
     const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cusine=${name}`);
     const recipes = await data.json();
-    setCusine(recipes.result);
+    setCusine(recipes.results);
   }
 
   useEffect(() => {
     getCusine(params.type);
   },[params.type])
   return (
-    <div>
-
-    </div>
+    <Grid>
+      {cusine.map(item => {
+        return(
+          <Card key={item.id}>
+          <img src={item.image} alt="" />
+          <h4>{item.title}</h4>
+        </Card>
+        )
+      })}
+    </Grid>
   )
 }
+
+const Grid = styled.div`
+grid-template-columns: 1fr 1fr 1fr; 
+grid-template-rows: 1fr 1fr 1fr; 
+gap: 3rem;
+  `;
+
+const Card = styled.div`
+  img{
+    widht:100%;
+    border-radius: 2rem;
+  }
+  
+  a{
+    text-decoration:none;
+  }
+
+  h4{
+    text-align:center;
+    padding: 1rem;
+  }
+
+
+  `;
+
 
 export default Cusine
